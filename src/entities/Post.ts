156c60db7bcb53cf94fbc8ddbe52baa514022101
Rinat Khusainov <khusainovrm@yandex.ru>
2020-12-18 18:06:51 +0300

    feat: connect mikro-orm to graphql SETUP

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,18 +1,22 @@
-import {Entity, PrimaryKey, Property} from "@mikro-orm/core";
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field()
+  @PrimaryKey()
+  id!: number;
 
-    @PrimaryKey()
-    id!: number;
+  @Field(() => String)
+  @Property({ type: "date" })
+  createdAt = new Date();
 
-    @Property({type: 'date'})
-    createdAt = new Date();
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
 
-    @Property({ type: 'date', onUpdate: () => new Date() })
-    updatedAt = new Date();
-
-    @Property({type: 'text'})
-    title!: string;
-
+  @Field()
+  @Property({ type: "text" })
+  title!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import {MikroORM} from '@mikro-orm/core'
-import microConfig from './mikro-orm.config'
-import express from 'express'
-import {ApolloServer} from 'apollo-server-express'
-import {buildSchema} from 'type-graphql'
-import {HelloResolver} from "./resolvers/hello";
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
-const main = async() => {
-    const orm = await MikroORM.init(microConfig)
-    await orm.getMigrator().up()
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-    const app = express()
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver],
-            validate: false
-        })
-    })
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
 
-    apolloServer.applyMiddleware({app})
-    app.get('/', (_,res) => res.send('hello'))
-    app.listen(4000, () => console.log('Server started on localhost:4000'))
+  apolloServer.applyMiddleware({ app });
+  app.get("/", (_, res) => res.send("hello"));
+  app.listen(4000, () => console.log("Server started on localhost:4000"));
+};
 
-}
-
- main().catch(console.error)
+main().catch(console.error);
